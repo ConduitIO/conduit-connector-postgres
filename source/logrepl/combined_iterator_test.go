c3edc9e344edf71e869c532ed3096ae6abe6bb9f
dylan_lott <lott.dylan@gmail.com>
2022-04-11 16:41:16 -0600

    WIP adding failing test structure

--- source/logrepl/combined_iterator_test.go
+++ source/logrepl/combined_iterator_test.go
@@ -2,43 +2,62 @@ package logrepl
 
 import (
 	"context"
-	"reflect"
+	"errors"
 	"testing"
 
+	"github.com/conduitio/conduit-connector-postgres/test"
 	sdk "github.com/conduitio/conduit-connector-sdk"
+	"github.com/matryer/is"
 )
 
-func TestCombinedIterator_Next(t *testing.T) {
-	type fields struct {
-		cdc  *CDCIterator
-		snap *SnapshotIterator
-	}
-	type args struct {
-		ctx context.Context
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    sdk.Record
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := &CombinedIterator{
-				cdc:  tt.fields.cdc,
-				snap: tt.fields.snap,
+func TestCombinedIterator(t *testing.T) {
+	is := is.New(t)
+	pool := test.ConnectPool(context.Background(), t, test.RepmgrConnString)
+
+	t.Run("should switch into CDC mode when snapshot finished", func(t *testing.T) {
+		ctx := context.Background()
+
+		table := test.SetupTestTable(ctx, t, pool)
+		// TODO: create test snapshot
+
+		conn, err := pool.Acquire(ctx)
+		is.NoErr(err)
+		ci, err := NewCombinedIterator(ctx, conn.Conn(), Config{})
+		is.NoErr(err)
+
+		for i := 0; i < 5; i++ {
+			// 5th call should return ErrSnapshotComplete
+			_, err := ci.Next(ctx)
+			if err != nil {
+				is.True(errors.Is(err, ErrSnapshotComplete))
+				break
 			}
-			got, err := c.Next(tt.args.ctx)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("CombinedIterator.Next() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("CombinedIterator.Next() = %v, want %v", got, tt.want)
-			}
+		}
+
+		// insert a record so that CDC reads something off
+		insert := `INSERT INTO %s (id, column1, column2, column3)
+			VALUES (6, 'bizz', 456, false)`
+		_, err = pool.Exec(ctx, insert)
+		is.NoErr(err)
+
+		// should detect next CDC record
+		rec, err := ci.Next(ctx)
+		is.NoErr(err)
+
+		is.Equal(rec, sdk.Record{
+			Key: sdk.StructuredData{"id": int64(6)},
+			Metadata: map[string]string{
+				"table":  table,
+				"action": "insert",
+			},
+			Payload: sdk.StructuredData{
+				"id":      int64(6),
+				"column1": "bizz",
+				"column2": int32(456),
+				"column3": false,
+				"key":     nil,
+			},
 		})
-	}
+		// assert that it equals the record we made above
+	})
 }
