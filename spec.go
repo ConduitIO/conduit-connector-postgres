c9b6b723c2f0c0d6dee33af50d458340f7a17187
Lovro Mažgon <lovro.mazgon@gmail.com>
2022-09-21 13:49:05 +0200

    fixes

--- spec.go
+++ spec.go
@@ -21,7 +21,7 @@ import (
 // version is set during the build process (i.e. the Makefile).
 // It follows Go's convention for module version, where the version
 // starts with the letter v, followed by a semantic version.
-var version = "dev"
+var version = "v0.0.0-dev"
 
 func Specification() sdk.Specification {
 	return sdk.Specification{
