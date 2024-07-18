798723177139db23651f3bba555d2ed2d1bd687a
Lyubo Kamenov <lyubo@meroxa.io>
2024-07-18 16:46:40 -0400

    Refresh connection before starting replication.

--- source/logrepl/combined_test.go
+++ source/logrepl/combined_test.go
@@ -165,6 +165,16 @@ func TestCombinedIterator_Next(t *testing.T) {
 
 	expectedRecords := testRecords()
 
+	// interrupt repl connection
+	var terminated bool
+	is.NoErr(pool.QueryRow(ctx, fmt.Sprintf(
+		`SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE
+			query ILIKE '%%CREATE_REPLICATION_SLOT %s%%' and pid <> pg_backend_pid()
+		`,
+		table,
+	)).Scan(&terminated))
+	is.True(terminated)
+
 	// compare snapshot
 	for id := 1; id < 5; id++ {
 		t.Run(fmt.Sprint("next_snapshot", id), func(t *testing.T) {
