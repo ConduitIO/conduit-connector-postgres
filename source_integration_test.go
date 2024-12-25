df71d711a410714d0c99c40aa0423bd9999fb0ce
Haris Osmanagic <haris@meroxa.io>
2024-12-25 18:38:53 +0100

    improve tests

--- source_integration_test.go
+++ source_integration_test.go
@@ -16,8 +16,11 @@ package postgres
 
 import (
 	"context"
+	"fmt"
 	"testing"
+	"time"
 
+	"github.com/conduitio/conduit-commons/opencdc"
 	"github.com/conduitio/conduit-connector-postgres/source/logrepl"
 	"github.com/conduitio/conduit-connector-postgres/test"
 	"github.com/matryer/is"
@@ -25,9 +28,9 @@ import (
 
 func TestSource_Read(t *testing.T) {
 	is := is.New(t)
-	ctx := test.Context(t)
-	conn := test.ConnectSimple(ctx, t, test.RepmgrConnString)
-	tableName := test.SetupTestTable(ctx, t, conn)
+	ctx, _ := context.WithTimeout(test.Context(t), 3000*time.Second)
+
+	tableName := prepareSourceIntegrationTestTable(ctx, t)
 	slotName := "conduitslot1"
 	publicationName := "conduitpub1"
 
@@ -56,25 +59,84 @@ func TestSource_Read(t *testing.T) {
 		is.NoErr(s.Teardown(ctx))
 	})
 
-	for i := 0; i < 4; i++ {
-		gotRecord, err := s.Read(ctx)
-		is.NoErr(err)
+	gotRecord, err := s.Read(ctx)
+	is.NoErr(err)
+	err = s.Ack(ctx, gotRecord.Position)
+	is.NoErr(err)
+	assertRecordOK(is, tableName, gotRecord)
 
-		is.True(gotRecord.Key != nil)
-		is.True(gotRecord.Payload.After != nil)
+	insertRow(ctx, t, tableName, 2)
 
-		payloadSchemaSubject, err := gotRecord.Metadata.GetPayloadSchemaSubject()
-		is.NoErr(err)
-		is.Equal(tableName+"_payload", payloadSchemaSubject)
-		payloadSchemaVersion, err := gotRecord.Metadata.GetPayloadSchemaVersion()
-		is.NoErr(err)
-		is.Equal(1, payloadSchemaVersion)
+	gotRecord, err = s.Read(ctx)
+	is.NoErr(err)
+	err = s.Ack(ctx, gotRecord.Position)
+	is.NoErr(err)
+	assertRecordOK(is, tableName, gotRecord)
+}
 
-		keySchemaSubject, err := gotRecord.Metadata.GetKeySchemaSubject()
+func assertRecordOK(is *is.I, tableName string, gotRecord opencdc.Record) {
+	is.True(gotRecord.Key != nil)
+	is.True(gotRecord.Payload.After != nil)
+
+	payloadSchemaSubject, err := gotRecord.Metadata.GetPayloadSchemaSubject()
+	is.NoErr(err)
+	is.Equal(tableName+"_payload", payloadSchemaSubject)
+	payloadSchemaVersion, err := gotRecord.Metadata.GetPayloadSchemaVersion()
+	is.NoErr(err)
+	is.Equal(1, payloadSchemaVersion)
+
+	keySchemaSubject, err := gotRecord.Metadata.GetKeySchemaSubject()
+	is.NoErr(err)
+	is.Equal(tableName+"_key", keySchemaSubject)
+	keySchemaVersion, err := gotRecord.Metadata.GetKeySchemaVersion()
+	is.NoErr(err)
+	is.Equal(1, keySchemaVersion)
+}
+
+func prepareSourceIntegrationTestTable(ctx context.Context, t *testing.T) string {
+	is := is.New(t)
+
+	conn := test.ConnectSimple(ctx, t, test.RepmgrConnString)
+	table := test.RandomIdentifier(t)
+
+	query := fmt.Sprintf(`
+		CREATE TABLE %s (
+		id bigserial PRIMARY KEY,
+		key bytea,
+		column1 varchar(256),
+		column2 integer,
+		column3 boolean,
+		column4 varchar(256) NOT NULL,
+		column5 integer NOT NULL,
+		column6 boolean NOT NULL
+	)`, table)
+	_, err := conn.Exec(ctx, query)
+	is.NoErr(err)
+
+	t.Cleanup(func() {
+		query := `DROP TABLE %s`
+		query = fmt.Sprintf(query, table)
+		_, err := conn.Exec(context.Background(), query)
 		is.NoErr(err)
-		is.Equal(tableName+"_key", keySchemaSubject)
-		keySchemaVersion, err := gotRecord.Metadata.GetKeySchemaVersion()
-		is.NoErr(err)
-		is.Equal(1, keySchemaVersion)
-	}
+	})
+
+	insertRow(ctx, t, table, 1)
+
+	return table
+}
+
+func insertRow(ctx context.Context, t *testing.T, table string, rowNumber int) {
+	is := is.New(t)
+	conn := test.ConnectSimple(ctx, t, test.RepmgrConnString)
+
+	query := fmt.Sprintf(
+		`INSERT INTO %s (key, column1, column2, column3, column4, column5, column6)
+		VALUES ('%v', null, null, null, 'foo-%v', %d, false)`,
+		table,
+		rowNumber,
+		rowNumber,
+		100+rowNumber,
+	)
+	_, err := conn.Exec(ctx, query)
+	is.NoErr(err)
 }
