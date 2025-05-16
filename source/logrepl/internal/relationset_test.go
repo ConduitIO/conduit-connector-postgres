7ff10f12316968de979c5bfa4a0bdeb776a49ccf
Haris Osmanagic <haris@meroxa.io>
2025-05-16 19:27:10 +0200

    Add failing integration test, upgrade version of Pg server

--- source/logrepl/internal/relationset_test.go
+++ source/logrepl/internal/relationset_test.go
@@ -15,8 +15,6 @@
 package internal
 
 import (
-	"context"
-	"fmt"
 	"math/big"
 	"net"
 	"net/netip"
@@ -50,9 +48,9 @@ func TestRelationSetAllTypes(t *testing.T) {
 
 	pool := test.ConnectPool(ctx, t, test.RepmgrConnString)
 
-	table := setupTableAllTypes(ctx, t, pool)
+	table := test.SetupTableAllTypes(ctx, t, pool)
 	_, messages := setupSubscription(ctx, t, pool, table)
-	insertRowAllTypes(ctx, t, pool, table)
+	test.InsertRowAllTypes(ctx, t, pool, table)
 
 	var rel *pglogrepl.RelationMessage
 	var ins *pglogrepl.InsertMessage
@@ -99,163 +97,6 @@ func TestRelationSetAllTypes(t *testing.T) {
 		is.NoErr(err)
 		isValuesAllTypesStandalone(is, values)
 	})
-}
-
-// setupTableAllTypes creates a new table with all types and returns its name.
-func setupTableAllTypes(ctx context.Context, t *testing.T, conn test.Querier) string {
-	is := is.New(t)
-	table := test.RandomIdentifier(t)
-	query := `
-		CREATE TABLE %s (
-		  col_bit           bit(8),
-		  col_varbit        varbit(8),
-		  col_boolean       boolean,
-		  col_box           box,
-		  col_bytea         bytea,
-		  col_char          char(3),
-		  col_varchar       varchar(10),
-		  col_cidr          cidr,
-		  col_circle        circle,
-		  col_date          date,
-		  col_float4        float4,
-		  col_float8        float8,
-		  col_inet          inet,
-		  col_int2          int2,
-		  col_int4          int4,
-		  col_int8          int8,
-		  col_interval      interval,
-		  col_json          json,
-		  col_jsonb         jsonb,
-		  col_line          line,
-		  col_lseg          lseg,
-		  col_macaddr       macaddr,
-		  col_macaddr8      macaddr8,
-		  col_money         money,
-		  col_numeric       numeric(8,2),
-		  col_path          path,
-		  col_pg_lsn        pg_lsn,
-		  col_pg_snapshot   pg_snapshot,
-		  col_point         point,
-		  col_polygon       polygon,
-		  col_serial2       serial2,
-		  col_serial4       serial4,
-		  col_serial8       serial8,
-		  col_text          text,
-		  col_time          time,
-		  col_timetz        timetz,
-		  col_timestamp     timestamp,
-		  col_timestamptz   timestamptz,
-		  col_tsquery       tsquery,
-		  col_tsvector      tsvector,
-		  col_uuid          uuid,
-		  col_xml           xml
-		)`
-	query = fmt.Sprintf(query, table)
-	_, err := conn.Exec(ctx, query)
-	is.NoErr(err)
-
-	t.Cleanup(func() {
-		query := `DROP TABLE %s`
-		query = fmt.Sprintf(query, table)
-		_, err := conn.Exec(context.Background(), query)
-		is.NoErr(err)
-	})
-	return table
-}
-
-func insertRowAllTypes(ctx context.Context, t *testing.T, conn test.Querier, table string) {
-	is := is.New(t)
-	query := `
-		INSERT INTO %s (
-		  col_bit,
-		  col_varbit,
-		  col_boolean,
-		  col_box,
-		  col_bytea,
-		  col_char,
-		  col_varchar,
-		  col_cidr,
-		  col_circle,
-		  col_date,
-		  col_float4,
-		  col_float8,
-		  col_inet,
-		  col_int2,
-		  col_int4,
-		  col_int8,
-		  col_interval,
-		  col_json,
-		  col_jsonb,
-		  col_line,
-		  col_lseg,
-		  col_macaddr,
-		  col_macaddr8,
-		  col_money,
-		  col_numeric,
-		  col_path,
-		  col_pg_lsn,
-		  col_pg_snapshot,
-		  col_point,
-		  col_polygon,
-		  col_serial2,
-		  col_serial4,
-		  col_serial8,
-		  col_text,
-		  col_time,
-		  col_timetz,
-		  col_timestamp,
-		  col_timestamptz,
-		  col_tsquery,
-		  col_tsvector,
-		  col_uuid,
-		  col_xml
-		) VALUES (
-		  B'00000001',                                -- col_bit
-		  B'00000010',                                -- col_varbit
-		  true,                                       -- col_boolean
-		  '(3,4),(5,6)',                              -- col_box
-		  '\x07',                                     -- col_bytea
-		  '8',                                        -- col_char
-		  '9',                                        -- col_varchar
-		  '192.168.100.128/25',                       -- col_cidr
-		  '<(11,12),13>',                             -- col_circle
-		  '2022-03-14',                               -- col_date
-		  15,                                         -- col_float4
-		  16.16,                                      -- col_float8
-		  '192.168.0.17',                             -- col_inet
-		  32767,                                      -- col_int2
-		  2147483647,                                 -- col_int4
-		  9223372036854775807,                        -- col_int8
-		  '18 seconds',                               -- col_interval
-		  '{"foo": "bar"}',                            -- col_json
-		  '{"foo": "baz"}',                            -- col_jsonb
-		  '{19,20,21}',                               -- col_line
-		  '((22,23),(24,25))',                        -- col_lseg
-		  '08:00:2b:01:02:26',                        -- col_macaddr
-		  '08:00:2b:01:02:03:04:27',                  -- col_macaddr8
-		  '$28',                                      -- col_money
-		  '292929.29',                                -- col_numeric
-		  '[(30,31),(32,33),(34,35)]',                -- col_path
-		  '36/37',                                    -- col_pg_lsn
-		  '10:20:10,14,15',                           -- col_pg_snapshot
-		  '(38,39)',                                  -- col_point
-		  '((40,41),(42,43),(44,45))',                -- col_polygon
-		  32767,                                      -- col_serial2
-		  2147483647,                                 -- col_serial4
-		  9223372036854775807,                        -- col_serial8
-		  'foo bar baz',                              -- col_text
-		  '04:05:06.789',                             -- col_time
-		  '04:05:06.789-08',                          -- col_timetz
-		  '2022-03-14 15:16:17',                      -- col_timestamp
-		  '2022-03-14 15:16:17-08',                   -- col_timestamptz
-		  'fat & (rat | cat)',                        -- col_tsquery
-		  'a fat cat sat on a mat and ate a fat rat', -- col_tsvector
-		  'bd94ee0b-564f-4088-bf4e-8d5e626caf66',     -- col_uuid
-		  '<foo>bar</foo>'                            -- col_xml
-		)`
-	query = fmt.Sprintf(query, table)
-	_, err := conn.Exec(ctx, query)
-	is.NoErr(err)
 }
 
 func isValuesAllTypes(is *is.I, got map[string]any) {
